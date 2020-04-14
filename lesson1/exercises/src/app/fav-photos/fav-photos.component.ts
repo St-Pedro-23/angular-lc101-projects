92232feb98b48cd11bdd7c7a109bcf6008b4e648
Pedro Rios <st.pedro@gmail.com>
2020-04-14 11:36:30 -0500

    commiting part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some of My Favorite Pics';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://vignette.wikia.nocookie.net/robotech/images/2/27/SKull_one_in_action.png/revision/latest/scale-to-width-down/340?cb=20150831160402';
+  image3 = 'https://wh40k.lexicanum.com/mediawiki/images/thumb/7/72/SangTerra.jpg/370px-SangTerra.jpg';
 
   constructor() { }
 
